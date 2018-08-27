1648ec24c34c86ccfe4533aa22f02710dd4cec89
manuzhang <owenzhang1990@gmail.com>
2018-08-27 21:43:26 +0800

    Handle more HTTP status codes

--- htmlproofer/plugin.py
+++ htmlproofer/plugin.py
@@ -1,8 +1,16 @@
 from mkdocs.plugins import BasePlugin
 
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
+import urllib3
+import uuid
 
+
+URL_TIMEOUT = 10.0
+_URL_BOT_ID = 'Bot {id}'.format(id=str(uuid.uuid4()))
+URL_HEADERS = {'User-Agent': _URL_BOT_ID}
+
+urllib3.disable_warnings()
 
 class HtmlProoferPlugin(BasePlugin):
 
@@ -11,17 +19,38 @@ class HtmlProoferPlugin(BasePlugin):
         local = ['localhost', '127.0.0.1']
         for a in soup.find_all('a', href=True):
             url = a['href']
-            for local in ('localhost', '127.0.0.1', 'app_server'):
-                if url.startswith('http://' + local):
-                    return
-            if url.startswith('http'):
-                print('Checking url ' + url)
-                try:
-                    status = urllib.request.urlopen(url, timeout=10).getcode()
-                    if status != 200:
-                        print('Bad url ' + url)
-                except:
-                    print('Failed to open url ' + url)
+            clean_url, url_status = self.get_url_status(url)
+            if self.bad_url(url_status) is True:
+                print('{}: {}\n'.format(clean_url, url_status))
 
 
 
+    def get_url_status(self, url):
+        for local in ('localhost', '127.0.0.1', 'app_server'):
+            if url.startswith('http://' + local):
+                return (url, 0)
+        clean_url = url.strip('?.')
+        if clean_url.startswith('http://'):
+            try:
+                response = requests.get(
+                    clean_url, verify=False, timeout=URL_TIMEOUT,
+                    headers=URL_HEADERS)
+                return (clean_url, response.status_code)
+            except requests.exceptions.Timeout:
+                return (clean_url, 504)
+            except requests.exceptions.ConnectionError:
+                return (clean_url, -1)
+        else:
+            return (url, 0)
+
+
+    def bad_url(self, url_status):
+        if url_status == -1:
+            return True
+        elif url_status == 401 or url_status == 403:
+            return False
+        elif url_status == 503:
+            return False
+        elif url_status >= 400:
+            return True
+        return False
