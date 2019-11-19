8dedabbcae02fafe9a5d31a90004088e28d3bf01
manuzhang <owenzhang1990@gmail.com>
2019-11-20 07:41:44 +0800

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='mkdocs-htmlproofer-plugin',
-    version='0.0.3.dev',
+    version='0.0.3',
     description='A MkDocs plugin that injects the mkdocs.yml extra variables into the markdown template',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
