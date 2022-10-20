7007a84147a93a839c7f3a76287c3860ff4c6a83
johnthagen <johnthagen@gmail.com>
2022-10-20 08:05:46 -0400

    Support Python 3.10

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     packages=find_packages(exclude=['*.tests']),
     entry_points={
